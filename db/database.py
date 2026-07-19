dd1819b00ce592b9ecb394c89ebf98621fb95795
Endr1x_ <yje.ispolzuetca@gmail.com>
2026-07-19 20:07:37 +0000

    Fix get_db import error in database.py

--- db/database.py
+++ db/database.py
@@ -1,11 +1,6 @@
 import sqlite3
-from config import DB_NAME
-from db import queries
+from config import DATABASE
 
-def init_db():
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-    cursor.execute(queries.CREATE_USERS_TABLE)
-    cursor.execute(queries.CREATE_TASKS_TABLE)
-    conn.commit()
-    conn.close()
+def get_db():
+    conn = sqlite3.connect(DATABASE)
+    return conn
